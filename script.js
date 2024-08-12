cf4d61edf0b36f7315e66d5c73e2b5b0611d8485
sidmuzammil <sidhuxnm@gmail.com>
2024-08-13 00:34:14 +0530

    Completation

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const takeSelfieButton = document.querySelector('.left-sid button');
+const takeSelfieButton = document.querySelector('.left-side button');
 const heroFaceElement = document.querySelector('.HeroFace');
 const tickGif = document.querySelector('.tickGif');
 const detection = document.querySelector('.detection');
 const detectionGreen = document.querySelector('.DetectionGreen');
+const notification = document.querySelector('.notification');
 
 takeSelfieButton.addEventListener('click', function() {
     if (takeSelfieButton.textContent === 'Take Selfie') {
@@ -11,18 +12,28 @@ takeSelfieButton.addEventListener('click', function() {
         tickGif.style.display = 'block';
         detection.style.display = 'none';
         detectionGreen.style.display = 'block';
+        takeSelfieButton.style.backgroundColor ="#fff"
+        takeSelfieButton.style.border = "2px solid #4e9459";
+        takeSelfieButton.style.color ="#4e9459"
 
         setTimeout(() => {
             detectionGreen.style.opacity = '1';
         }, 10);
 
         takeSelfieButton.textContent = 'Take Another';
+        notification.style.display = 'block';
+          setTimeout(() => {
+              notification.style.display = 'none';
+          }, 1000);
     } else {
         heroFaceElement.classList.remove('animate-border');
         tickGif.style.display = 'none';
         detection.style.display = 'block';
         detectionGreen.style.display = 'none';
         detectionGreen.style.opacity = '0';
+        takeSelfieButton.style.backgroundColor ="#4e9459"
+        takeSelfieButton.style.color ="#ffff"
+
 
         takeSelfieButton.textContent = 'Take Selfie';
     }
@@ -31,8 +42,10 @@ takeSelfieButton.addEventListener('click', function() {
 
 
 
+
+
 function handleResponsiveLayout() {
-  const leftSide = document.querySelector('.left-sid');
+  const leftSide = document.querySelector('.left-side');
   const rightSide = document.querySelector('.Right-side');
   const detectionGreen = document.querySelector('.DetectionGreen');
 
@@ -50,7 +63,7 @@ function handleResponsiveLayout() {
       // Add opacity transition for left side
       leftSide.style.opacity = '1'; // Set opacity to 1
       leftSide.style.transition = 'opacity 0.5s ease-in-out'; // Smooth transition
-    }, 5000);
+    }, 3000);
   } else {
     // Reset to default when screen is larger than 1025px
     rightSide.style.display = 'flex';
